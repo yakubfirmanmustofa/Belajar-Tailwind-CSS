5c7b4cda262a1d560296880d0179763a86c0e04b
yakubfirmanmustofa <113831481+yakubfirmanmustofa@users.noreply.github.com>
2024-04-05 00:37:30 +0700

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,4 @@
+function Menu(e){
+    let list = document.querySelector('ul');
+    e.name === 'menu' ? (e.name = "close",list.classList.add('top-[60px]') , list.classList.add('opacity-100')) :( e.name = "menu" ,list.classList.remove('top-[60px]'),list.classList.remove('opacity-100'))
+}
